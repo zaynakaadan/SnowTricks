19f8e203686de0b97e2a0f11326f547fc0703949
ZAYNA <zk10-91@hotmail.com>
2023-06-30 09:31:26 +0200

    add google mailer

--- src/Service/JWTService.php
+++ src/Service/JWTService.php
@@ -2,6 +2,8 @@
 
 namespace App\Service;
 
+use DateTimeImmutable;
+
 class JWTService
 {
     //Génère le token
@@ -11,6 +13,16 @@ class JWTService
         if($validity <= 0){
             return "";
         }
+        $now = new DateTimeImmutable();
+        $exp = $now->getTimestamp() + $validity;
+
+        $payload['iat'] = $now->getTimestamp();
+        $payload['exp'] = $exp;
+
+        //encode en base64
+        $base64Header = base64_encode(json_encode($header));
+        $base64Payload = base64_encode(json_encode($payload));
+        
         return $jwt;
     }
 }
