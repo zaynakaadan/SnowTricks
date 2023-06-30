9c9a5ef796140bf4da3f832f64b58163c0ae8e63
ZAYNA <zk10-91@hotmail.com>
2023-06-30 11:05:09 +0200

    generation of jwt

--- src/Service/JWTService.php
+++ src/Service/JWTService.php
@@ -8,6 +8,14 @@ class JWTService
 {
     //Génère le token
 
+    /**
+     * Génération du JWT
+     * @param array $header 
+     * @param array $payload 
+     * @param string $secret 
+     * @param int $validity 
+     * @return string 
+     */
     public function generate(array $header, array $payload, string $secret, int $validity = 10800): string
     {
         if($validity <= 0){
