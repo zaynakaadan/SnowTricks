966e58d87a29d778d13a5907fb7242bb843b82b4
ZAYNA <zk10-91@hotmail.com>
2023-07-16 02:25:31 +0200

    adding datetime

--- src/Controller/TricksController.php
+++ src/Controller/TricksController.php
@@ -2,11 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\MyTrait\CreatedAtTrait;
 use App\Entity\Tricks;
-
+use DateTimeInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @Route("/tricks", name="tricks_")
@@ -17,7 +19,7 @@ class TricksController extends AbstractController
      * @Route("/", name="index")
      */
         public function index(): Response
-        {
+        {            
             return $this->render('tricks/index.html.twig');
         }
 
@@ -25,9 +27,11 @@ class TricksController extends AbstractController
     /**        
      * @Route("/{slug}", name="details")
      */      
-      public function details(Tricks $trick): Response
+      public function details(Tricks $trick, UserInterface $ui): Response
       {
+        $currentDateTime = $trick->getCreatedAt()->format('Y-m-d H:i:s');
         
-        return $this->render('tricks/details.html.twig', compact('trick'));
+        $user = $ui->getUserIdentifier();
+        return $this->render('tricks/details.html.twig', compact('trick', 'user', 'currentDateTime'));
       }
     }    
