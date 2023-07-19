441e92cad3743158d864448835c465d678c6513c
ZAYNA <zk10-91@hotmail.com>
2023-07-19 09:18:54 +0200

    add styling

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -6,3 +6,22 @@ document.addEventListener('DOMContentLoaded', function() {
         interval: 5000 // Set the interval time between slide transitions (in milliseconds)
     });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const scrollUpBtn = document.getElementById('scroll-up');
+    // Scroll to the top of the page when the up arrow is clicked
+    scrollUpBtn.addEventListener('click', () => {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    });    
+    // Show/hide the scroll-up arrow based on the scroll position
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 100) {
+            scrollUpBtn.style.display = 'block';
+        } else {
+            scrollUpBtn.style.display = 'none';
+        }
+    });    
+});
+
+
+
