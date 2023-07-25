dddcf099c8028adbb8a49d499b420de9de8dcb19
ZAYNA <zk10-91@hotmail.com>
2023-07-25 10:20:02 +0200

    add unique name

--- src/Entity/Categories.php
+++ src/Entity/Categories.php
@@ -23,7 +23,7 @@ class Categories
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=100)
+     * @ORM\Column(type="string", length=100, unique=true)
      */
     private $name;
 
